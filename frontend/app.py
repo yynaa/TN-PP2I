ac0cea9a017f43256399e0dcd7f532b2650090b7
yynaa <xykaecutie@gmail.com>
2023-11-15 18:30:42 +0100

    flask setup

---
+++ frontend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return '<h1>ULTRA MEGA SUPER WHEELCHAIR MAN SIMULATOR 2019</h1>'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080, debug=True)
