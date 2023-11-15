dcec60bc9c808995857a8c2d319c5c69fb11f4d7
yynaa <xykaecutie@gmail.com>
2023-11-15 19:55:25 +0100

    marker support (parses from fictional database)

--- frontend/app.py
+++ frontend/app.py
@@ -13,5 +13,13 @@ def mapLoader():
     """
     return render_template('map.html')
 
+@app.route('/api/buildings')
+def getMapData():
+    """
+    Returns the map data
+    """
+    # this is test data
+    return []
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
