ca0e7b1f89229083c092f0da0ef681eba81a5e8c
GEORGEON Gautier <gautier.georgeon@telecomnancy.eu>
2023-11-19 16:07:56 +0100

    Login pages synchronized with flask Footer deleted in index.html

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import folium
+import sys
+
+from backend.db.functions import *
 
 app = Flask(__name__)
 
@@ -11,7 +15,16 @@ def mapLoader():
     """
     Loads the map page
     """
-    return render_template('map.html')
+    m = folium.Map(location=[48.689, 6.2], zoom_start=13)
+
+    for building in fetch_Data("backend/db/database.db", "Buildings"):
+        folium.Marker(
+            location=[building["GPS_lat"], building["GPS_long"]],
+            popup=building["building_name"],
+            icon=folium.Icon(color='red', icon='info-sign')
+        ).add_to(m)
+
+    return m.get_root().render()
 
 @app.route('/api/buildings')
 def getMapData():
@@ -21,5 +34,48 @@ def getMapData():
     # this is test data
     return []
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=8080, debug=True)
+def getMapData():
+    """
+    Returns the map data
+    """
+    # this is test data
+    return []
+
+############################################################################
+# LOGIN PART
+
+@app.route('/login', methods = ['GET'])
+def login():
+    return render_template("welcome/index.html")
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template("welcome/register.html")
+
+@app.route('/forgot', methods=['GET'])
+def forgot():
+    return render_template("welcome/forgot_mail.html")
+
+#########################
+
+# THESES HTML PAGES ARE NOT PERMANENT, JUST HERE TO CHECK IF THE DB AND 
+# THE FRONT END STRUCTURE ARE WORKING
+
+@app.route('/post_login', methods=['POST'])
+def postlog():
+    "<p>Cond of connection will be implemented asap</p>"
+
+@app.route('/post_reg', methods=['POST'])
+def postreg():
+    "<p>Cond of connection will be implemented asap</p>"
+
+@app.route('/post_forgot', methods=['POST'])
+def postforgot():
+    "<p>Cond of connection will be implemented asap</p>"
+
+############################
+
+#########################################################################
+
+#if __name__ == '__main__':
+    
